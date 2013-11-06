29469e447df70666a98d8184171cb5f2c3f7e223
Andy Chase <asperous2@gmail.com>
2013-11-06 13:24:51 -0800

    Fix setup not working if readme does not exist.

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 import os
 from distutils.core import setup
 
-read = lambda file_name: open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+def readme_or_docstring(): 
+    path = os.path.join(os.path.dirname(__file__), 'readme.rst')
+    if os.path.isfile(path):
+        return open(path).read()
+    else:
+        import pipeless
+        return pipeless.__doc__
 
 setup(name='Pipeless',
       version='1.1',
       description='Simple pipelines building framework.',
-      long_description=read('readme.rst'),
+      long_description=readme_or_docstring(),
       author='Andy Chase',
       author_email='andy@asperous.us',
       url='http://asperous.github.io/pipeless',
